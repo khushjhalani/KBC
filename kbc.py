c223cc3904ce59f10cdd7fe730bfccb7b88a10ef
Khush Jhalani <khushjhalani.03@gmail.com>
2023-06-26 00:47:26 +0530

    Full working code

--- kbc.py
+++ kbc.py
@@ -39,27 +39,27 @@ winnings = 0
 
 def welcomeMsg():
     # Display the welcome message
-    print("Welcome to Kaun Banega Crorepati!\n\n\n")
+    print("\n\nWelcome to Kaun Banega Crorepati!\n")
 
     # Display the rules
-    print('''Rules:\n
-        1. There are 3 levels in this game: Easy, Medium and Hard.\n
-        2. Each level has 5 questions.\n
-        3. You will be given 4 options for each question.\n
-        4. You have to enter the option number to answer the question.\n
-        5. You will be awarded with the prize money mentioned below for each correct answer.\n
-        6. If you give a wrong answer, the game will be over and you will lose all the money you have won after the last checkpoint.\n
-        7. You can quit the game at any time by entering 'quit'.\n
-        8. There are 2 checkpoints in the game at clearing first two difficulty levels.\n
-        9. You will get 60 seconds to answer each question at first level and then no timelimit.\n
+    print('''Rules:\
+        \n1. There are 3 levels in this game: Easy, Medium and Hard.\
+        \n2. Each level has 5 questions.\
+        \n3. You will be given 4 options for each question.\
+        \n4. You have to enter the option number to answer the question.\
+        \n5. You will be awarded with the prize money mentioned below for each correct answer.\
+        \n6. If you give a wrong answer, the game will be over and you will lose all the money you have won after the last checkpoint.\
+        \n7. You can quit the game at any time by entering 'quit'.\
+        \n8. There are 2 checkpoints in the game at clearing first two difficulty levels.\
+        \n9. You will get 60 seconds to answer each question at first level and then no timelimit.\
         \n\n''')
 
-def get_user_input():
+def get_user_input(tl=60):
     start_time = time.time()
     user_input = ""
 
     while True:
-        if time.time() - start_time > 60:
+        if time.time() - start_time > tl:
             print("Time limit exceeded.")
             return False
         if msvcrt.kbhit():
@@ -85,7 +85,7 @@ def l1():
         if choice == False:
             print("OOPS! Time Limit exceeded.")
             return 'tle'
-        elif choice == 'quit':
+        elif choice.lower() == 'quit':
             print("You chose to quit the game...")
             winnings = prize[i-1]
             print(f"Your winning amount is: {winnings}")
@@ -100,24 +100,67 @@ def l1():
             return 'lost'
     return True
 
-
 def l2():
-    pass
-
+    global winnings
+    for i in range(1,6):
+        print(f"Question {i+5} is: ")
+        print(f"{medium_questions[i][0]}")
+        print(f"Your options are:")
+        for val in range(4):
+            print(f"{val+1} . {medium_questions[i][1][val]}")
+            
+        choice = input("Enter your choice: ")
+        if choice.lower() == 'quit':
+            print("You chose to quit the game...")
+            winnings = prize[i+5-1]
+            print(f"Your winning amount is: {winnings}")
+            exit()
+        elif choice == medium_questions[i][2]:
+            print("Congratulations! Your answer is Correct.")
+            winnings = prize[i+5]
+            print(f"Your current winning amount is {winnings}")
+        else:
+            print("OOPS! You entered wrong answer.")
+            return 'lost'
+    
+    return True            
+               
 def l3():
-    pass
-
+    global winnings
+    for i in range(1,6):
+        print(f"Question {i+10} is: ")
+        print(f"{hard_questions[i][0]}")
+        print(f"Your options are:")
+        for val in range(4):
+            print(f"{val+1} . {hard_questions[i][1][val]}")
+            
+        choice = input("Enter your choice: ")
+        if choice.lower() == 'quit':
+            print("You chose to quit the game...")
+            winnings = prize[i+10-1]
+            print(f"Your winning amount is: {winnings}")
+            exit()
+        elif choice == hard_questions[i][2]:
+            print("Congratulations! Your answer is Correct.")
+            winnings = prize[i+10]
+            print(f"Your current winning amount is {winnings}")
+        else:
+            print("OOPS! You entered wrong answer.")
+            return 'lost'
+    return True
+            
 
 def playGame():
     print("Let's start the game!\nYou are at level 1 right now\n\n")
     global winnings
-    c1 = False
-    c2 = False
+    c1 = 10000
+    c2 = 320000
     
     
     a = l1()
     if a==True:
-        c1 = True
+        print("Congrats!! You cleared level 1 and your checkpoint 1 is now set.\nTimer is now removed and there is no time limit")
+        print("Let's start level 2\n")
     elif a=='tle' or a=='lost':
         print(f"You failed to give correct answer on time and thus have not won any amount.")
         winnings = 0
@@ -125,9 +168,33 @@ def playGame():
     else:
         #Implement error logging for keeping track of unforeseen circumstances 
         print("Something unexpected occurred...")
-
-
         
+    
+    a = l2()
+    if a==True:
+        print("Congrats!! You cleared level 2 and your checkpoint 2 is now set.\n")
+        print("Let's start level 3\n")
+    elif a=='lost':
+        print(f"You failed to give correct answer")
+        winnings = c1
+        print(f"Your winning amount according to your checkpoint is: {winnings}")
+        exit()
+    else:
+        #Implement error logging for keeping track of unforeseen circumstances 
+        print("Something unexpected occurred...")
+       
+       
+    a=l3()
+    if a==True:
+        print('''\n\n\n!!!!!!!!!!!!CONGRATULAIONS!!!!!!!!!!!!!!!\nYou have completed the game and you win 1,00,00,000.\nToh kya kariyega aap itni dhanrashi ka?''')
+        exit()
+    elif a=='lost':
+        winnings = c2
+        print(f"Your winning amount according to your checkpoint is: {winnings}")
+        exit()
+    else:
+        #Implement error logging for keeping track of unforeseen circumstances 
+        print("Something unexpected occurred...")       
         
         
     
@@ -139,4 +206,4 @@ if __name__=="__main__":
     if a == 'q':
         exit()
     playGame()
-    
+    exit()
